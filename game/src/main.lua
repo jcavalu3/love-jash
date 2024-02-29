423a713618083dfc4edd8303430f109da90ae243
Josh Cavaluzzi <shapopie12@gmail.com>
2024-02-29 00:32:24 -0500

    sheepolution tutorial, up to ch 5

---
+++ game/src/main.lua
@@ -0,0 +1,13 @@
+io.stdout:setvbuf("no")
+
+function love.load()
+  x = 100
+end
+
+function love.update(dt)
+  x = x + 100 * dt
+end
+
+function love.draw()
+  love.graphics.rectangle("line", x, 50, 200, 150)
+end
